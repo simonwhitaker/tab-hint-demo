d77136b6dccf9cd3736daae7ebd75588797b778f
Simon Whitaker <simon@goosoftware.co.uk>
2013-05-04 15:40:13 +0100

    Initial Commit

---
+++ Tab Hint Demo/GSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GSAppDelegate.h
+//  Tab Hint Demo
+//
+//  Created by Simon Whitaker on 04/05/2013.
+//  Copyright (c) 2013 Goo Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tab Hint Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tab Hint Demo
+//
+//  Created by Simon Whitaker on 04/05/2013.
+//  Copyright (c) 2013 Goo Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GSAppDelegate class]));
+    }
+}
